2594e7aa2f4f73eee6efa295792c5d002e742040
Tudor-Constantin Marian <103374046+TudorCMarian@users.noreply.github.com>
2025-01-04 18:58:30 +0200

    Add copyright banner to main.py

---
+++ code/main.py
@@ -0,0 +1,15 @@
+##########################################################################
+#                                                                        #
+#  Copyright:   (c) 2024, Tudor Marian, Plosnita Eduard, Paul Aflorei    #
+#  E-mail:      tudor-constantin.marian@student.tuiasi.ro                # 
+#               eduard.plosnita@student.tuiasi.ro                        #
+#               paul-bogdan.aflorei@student.tuiasi.ro                    #
+#  Description: Resource Optimization in Cloud Computing Using NSGA-II   #
+#                                                                        #
+#  This code and information are provided "as is" without warranty of    #
+#  any kind, either expressed or implied, including but not limited      #
+#  to the implied warranties of merchantability or fitness for a         #
+#  particular purpose. You are free to use this source code in your      #
+#  applications as long as the original copyright notice is included.    #
+#                                                                        #
+##########################################################################
